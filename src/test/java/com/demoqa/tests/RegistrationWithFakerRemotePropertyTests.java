da6724cb3ffaa5cd0c8cc28bd85508d03639bc6c
Elena Sokolova <>
2023-07-12 14:59:13 +0200

    removed unused imports

--- src/test/java/com/demoqa/tests/RegistrationWithFakerRemotePropertyTests.java
+++ src/test/java/com/demoqa/tests/RegistrationWithFakerRemotePropertyTests.java
@@ -1,13 +1,10 @@
 package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationPage;
-import com.demoqa.utils.RandomUtils;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.demoqa.utils.RandomUtils.*;
 import static io.qameta.allure.Allure.step;
 
 
@@ -20,22 +17,6 @@ public class RegistrationWithFakerRemotePropertyTests extends RemoteTestBase {
     @Tag("property")
     @DisplayName("Successful registration")
     void successfulRegistrationTest() {
-//        Faker faker = new Faker();
-//
-//        String firstName = faker.name().firstName(),
-//                lastName = faker.name().lastName(),
-//                userEmail = faker.internet().emailAddress(),
-//                gender = getRandomGender(),
-//                userNumber = faker.phoneNumber().subscriberNumber(10),
-//                day = String.valueOf(faker.number().numberBetween(0, 31)),
-//                month = getRandomMonth(),
-//                year = String.valueOf(RandomUtils.getRandomInt(1980, 1989)),
-//                subject = getRandomSubject(),
-//                hobbies = getRandomHobbies(),
-//                pictureName = "nature.jpg",
-//                currentAddress = faker.address().fullAddress(),
-//                state = getRandomState(),
-//                city = getRandomCity(state);
 
         step("Open form", () -> {
             registrationPage
